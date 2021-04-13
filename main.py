61583160c13824e0f80e18d69a70ce8609a23ae9
Gitea <gitea@fake.local>
2021-04-13 13:56:06 -0400

    Saving & more

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-import os, uuid
+import os, uuid, pickle
+from hashlib import sha1
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 
-def uuid_gen():
-  return uuid.uuid4().hex
-
+# Constants!
 UPLOAD_FOLDER = 'icons'
 ALLOWED_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif', '.tif', '.tiff', '.svg'}
 
+# Maximum upload size
 MEGABYTES = 5
 
+# uuid generation shortcut
+def uuid_gen():
+  return uuid.uuid4().hex
+
+# Determin if a file can be uploaded
+def allowed_file(filename):
+  # Use the safer os.path.splitext instead of string manipulation
+  return os.path.splitext(filename)[1].lower() in ALLOWED_EXTENSIONS
+
+def hash(data):
+    return sha1(data)
+
+# Configure flask
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MEGABYTES * 1024 * 1024
@@ -17,15 +30,12 @@ app.config['SECRET_KEY'] = uuid_gen()
 
 users = {'test': ['9f67db172e2d4db99095bfc4cf23f527.jpg', 'hehe']}
 
-def allowed_file(filename):
-  #Use the safer os.path.splitext instead of string manipulation
-  return os.path.splitext(filename)[1].lower() in ALLOWED_EXTENSIONS
-
+# Basic index
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
+# Fancy upload stuff!
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -49,7 +59,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             users[uname] = users[uname] + [filename] if uname in users.keys() else [filename]
-
+            
             return redirect(url_for('uploaded_icon',
                                     filename=filename))
     
@@ -60,28 +70,49 @@ def upload_file():
 def uploaded_icon(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], secure_filename(filename))
 
+# Set the favorite 
 @app.route('/set/<uname>/<icon>')
 def set_favorite(uname, icon):
+    # If the user exists then do stuff
     if uname in users.keys():
+        # If it's aleready set, then don't do anything
         if icon == users[uname][0]:
             return redirect("/")
+
         else:
+            # Other wise, if check for the icon
             if icon in users[uname]:
                 x = users[uname].remove(icon)
                 y = users[uname][0]
                 users[uname][0] = icon
                 users[uname].append(y)
                 return redirect("/")
+            # If not, then display an error
+            else:
+                return "ERROR NOT USER ICON"
+            
     else:
-        return "ERROR NO ICONS"
+        return "ERROR NO USER"
     
 
+# Chose the best icon
 @app.route('/user/<uname>')
 def user_icons(uname):
     return render_template("user.html", icons = users[uname], uname=uname)
 
+# Get the number 1 avatar from user
 @app.route('/avatar/<uname>')
 def get_best_avatar(uname):
     return send_from_directory(app.config['UPLOAD_FOLDER'], users[uname][0]) if uname in users.keys() else "ERROR USER NOT FOUND"
+
+# Pickle save and load, acessed using /save and /load
+
+@app.route('/save')
+def save():
+    pickle.dump(users, open( "save.p", "wb" ) )
+
+@app.route('/load')
+def load():
+    users = pickle.load( open( "save.p", "rb" ) )
 
 app.run(host='0.0.0.0', port=8080)
